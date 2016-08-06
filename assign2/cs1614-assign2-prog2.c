3944965dd3b16e2e71d4dc9a81fa0532653be1bb
Yash M Sawant <yashmrsawant@live.com>
2016-08-07 02:13:28 +0530

    Added Workable Home Directory

---
+++ assign2/cs1614-assign2-prog2.c
@@ -0,0 +1,15 @@
+/**************************************************************************
+Name : Yash M Sawant
+Roll : MTC - 1614
+Date : 06th Aug, 2016
+Desc : Conversion
+Acks : Using Command Line
+**************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	char* s = fgets(stdin);
+	fputs(s, stdout);
+	return 0;
+}
